c3dd5983b98cbd0abab565c2bebeee8f5ab7d33d
anhtuan4906 <anhtuan144906@gmail.com>
2024-11-26 14:20:23 +0700

    Add files via upload

---
+++ B2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    
+    int i=24;
+    int n;
+
+    while (n != i){
+        printf(" Hay doan so da cho ");
+        scanf("%d",&n);
+    }    
+    printf (" Ban da doan dung ");
+
+    return 0;
+}
